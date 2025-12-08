b2fcaae7ddb6a80a45a02ba1df05e2d739bb5722
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2025-12-08 22:50:27 +0300

    OOP ClassesPart1

---
+++ ClassesPart1/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setColor("red");
+        car.setDoors(3);
+        car.setConvertible(true);
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+    }
+}
