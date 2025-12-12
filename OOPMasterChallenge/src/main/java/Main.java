a83618f464b01cb64ca7fdced0fa08568078e7f1
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2025-12-12 17:55:40 +0300

    OOP MasterChallenge

---
+++ OOPMasterChallenge/src/main/java/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addTopppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+//        MealOrder meal2 = new MealOrder("turkey", "7-up", "chili");
+//        meal2.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        meal2.setDrinkSize("SMALL");
+//        meal2.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+    }
+
+}
