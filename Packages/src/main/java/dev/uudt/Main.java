ca31b07c6b5882df8fa6fb64f72d33a24b4283ab
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2025-12-16 13:25:59 +0300

    Packages

---
+++ Packages/src/main/java/dev/uudt/Main.java
@@ -0,0 +1,16 @@
+package dev.uudt;
+
+//import com.abc.first.*;
+import com.abc.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+
+}
