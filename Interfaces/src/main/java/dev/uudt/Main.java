6e74e56844b25f53f0cb993bab96be9f599fde92
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-03-17 00:13:13 +0300

    Interfaces Part1

--- Interfaces/src/main/java/dev/uudt/Main.java
+++ Interfaces/src/main/java/dev/uudt/Main.java
@@ -10,16 +10,31 @@ public class Main {
         FlightEnabled flier = bird;
         Trackable tracked = bird;
 
-//        bird.move();
-        animal.move();
-        // these methods are not declared in interfaces
-//        flier.move();
-//        tracked.move();
+        // Part 1
+////        bird.move();
+//        animal.move();
+//        // these methods are not declared in interfaces
+////        flier.move();
+////        tracked.move();
+//
+//        flier.takeOff();
+//        flier.fly();
+//        flier.land();
+//        tracked.track();
+
+        // Part 2
+        inFlight(flier);
+
+    }
+
+    private static void inFlight(FlightEnabled flier) {
 
         flier.takeOff();
         flier.fly();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
         flier.land();
-        tracked.track();
-
     }
+
 }
