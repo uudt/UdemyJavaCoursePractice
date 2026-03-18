480fba3fadaa6b6e7ca2a474f7959407214909e4
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-03-18 23:28:02 +0300

    Interfaces Part2

--- Interfaces/src/main/java/dev/uudt/Animal.java
+++ Interfaces/src/main/java/dev/uudt/Animal.java
@@ -1,5 +1,63 @@
 package dev.uudt;
 
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+
+        if (this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+
+}
+
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Satelite implements OrbitEarth {
+
+     public void achieveOrbit() {
+        System.out.println("Orbit achieved!");
+     }
+
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
 
     // fields in interfaces are public static final by default (declared as constants)
--- Interfaces/src/main/java/dev/uudt/Main.java
+++ Interfaces/src/main/java/dev/uudt/Main.java
@@ -1,5 +1,9 @@
 package dev.uudt;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -36,6 +40,22 @@ public class Main {
         System.out.printf("The truck travelled %.2f km or %.2f miles%n",
             kmsTravelled, milesTravelled);
 
+//        ArrayList<FlightEnabled> fliers = new ArrayList<>();
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+        // ArrayList implements List interface
+        // it's recommended to use interface types as the reference type (best practice)
+//        List<FlightEnabled> betterFliers = new ArrayList<>();
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -46,6 +66,27 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+
+    private static void triggerFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 
 }
