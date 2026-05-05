1d3827e0ae1c96aedbd3ee98490268042b12aa75
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-05-05 22:29:00 +0300

    Comparable vs Comparator practice

--- Comparing/src/main/java/dev/uudt/Main.java
+++ Comparing/src/main/java/dev/uudt/Main.java
@@ -1,6 +1,8 @@
 package dev.uudt;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 public class Main {
 
@@ -37,26 +39,53 @@ public class Main {
         Arrays.sort(students);
         System.out.println(Arrays.toString(students));
 
-        System.out.println("result = " + u.compareTo("Mary"));
+//        System.out.println("result = " + u.compareTo("Mary"));
+        System.out.println("result = " + u.compareTo(new Student("Mary")));
+
+        Comparator<Student> gpaSorter = new StudentGPAComparator();
+        Arrays.sort(students, gpaSorter.reversed());
+        System.out.println(Arrays.toString(students));
     }
 }
 
-class Student implements Comparable {
+class StudentGPAComparator implements Comparator<Student> {
 
-    private String name;
+    @Override
+    public int compare(Student o1, Student o2) {
+        return (o1.gpa + o1.name).compareTo(o2.gpa + o2.name);
+    }
+}
+
+class Student implements Comparable<Student> {
+
+    private static int LAST__ID = 1000;
+    private static Random random = new Random();
+    String name;
+    private int id;
+    protected double gpa;
+
 
     public Student(String name) {
         this.name = name;
+        id = LAST__ID++;
+        gpa = random.nextDouble(1.0, 4.0);
     }
 
     @Override
     public String toString() {
-        return name;
+//        return name;
+        return "%d = %s (%.2f)".formatted(id, name, gpa);
     }
 
+//    @Override
+//    public int compareTo(Object o) {
+//        Student other = (Student) o;
+//        return name.compareTo(other.name);
+//    }
+
     @Override
-    public int compareTo(Object o) {
-        Student other = (Student) o;
-        return name.compareTo(other.name);
+    public int compareTo(Student o) {
+//        return name.compareTo(o.name);
+        return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
     }
 }
