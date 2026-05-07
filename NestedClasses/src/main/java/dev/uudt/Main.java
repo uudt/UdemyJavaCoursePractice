a99ac31ceae001a8402935f60c32491bb461e3c5
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-05-07 14:42:50 +0300

    Non-static Nested Inner Classes

--- NestedClasses/src/main/java/dev/uudt/Main.java
+++ NestedClasses/src/main/java/dev/uudt/Main.java
@@ -2,6 +2,7 @@ package dev.uudt;
 
 import dev.uudt.domain.Employee;
 import dev.uudt.domain.EmployeeComparator;
+import dev.uudt.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +22,24 @@ public class Main {
 //        var comparator = new EmployeeComparator<>();
 //        employees.sort(comparator);
 
-        employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
-        for(Employee e : employees) {
+        employees.sort(new Employee.EmployeeComparator<>("yearStarted")
+            .reversed());
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+            new StoreEmployee(10015, "Meg", 2019, "Target"),
+            new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+            new StoreEmployee(10105, "Tom", 2020, "Macys"),
+            new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+            new StoreEmployee(10322, "Bud", 2016, "Target") ));
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
         }
 
