faf8f2071283ce467f58a6113c8b01b57a68a0e7
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-05-27 22:53:08 +0300

    practice nested classes

--- NestedClasses/src/main/java/dev/uudt/Main.java
+++ NestedClasses/src/main/java/dev/uudt/Main.java
@@ -43,5 +43,50 @@ public class Main {
             System.out.println(e);
         }
 
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+
     }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+//        lastName = "Latin";
+        newList.sort(null);
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee.originalInstance.getName() + " " +
+                dEmployee.pigLatinName);
+        }
+    }
+
 }
