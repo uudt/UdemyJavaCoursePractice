efdab8fc5f19424fe8747356e8efd4651b7e3029
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-06-04 23:48:59 +0300

    Lambda Expressions

--- LambdaExpressions/src/main/java/dev/uudt/Main.java
+++ LambdaExpressions/src/main/java/dev/uudt/Main.java
@@ -26,7 +26,13 @@ public class Main {
 //        prefix = "NATO";
 //        System.out.println(myString);
 
-        int result = calculator((a, b) -> a + b, 5, 2)
+        int result = calculator((var a, var b) -> a + b, 5, 2);
+//        int resultExtra = calculator((var a, var b) -> { return a + b; }, 5, 2);
+        var result2 = calculator((a, b) -> a / b, 10.0, 2.5);
+        var result3 = calculator(
+            (a, b) -> a.toUpperCase() + " " + b.toUpperCase(),
+            "Ralph", "Cranberry");
+
     }
 
     public static <T> T calculator(Operation<T> function, T value1, T value2) {
