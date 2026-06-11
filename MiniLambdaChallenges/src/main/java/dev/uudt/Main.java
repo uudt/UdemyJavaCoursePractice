9af9bee23c512d2bfb74717062f261c3cdd0cd51
Ulyana Tyuleneva <udtyuleneva@gmail.com>
2026-06-11 13:25:35 +0300

    Lambda Challenge 1

---
+++ MiniLambdaChallenges/src/main/java/dev/uudt/Main.java
@@ -0,0 +1,46 @@
+package dev.uudt;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Consumer<String> printWords = new Consumer<String>() {
+
+            @Override
+            public void accept(String sentence) {
+                String[] parts = sentence.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+        Consumer<String> printWordsLambda = sentence -> {
+            String[] parts = sentence.split(" ");
+            for (String part : parts) {
+                System.out.println(part);
+            }
+        };
+
+
+        printWords.accept("Let's split this up into an array");
+        printWordsLambda.accept("Let's split this up into an array");
+
+        Consumer<String> printWordsForEach = sentence -> {
+            String[] parts = sentence.split(" ");
+            Arrays.asList(parts).forEach(s -> System.out.println(s));
+        };
+
+        printWordsForEach.accept("Let's split this up into an array");
+
+        // The same lambda in 1 line
+        Consumer<String> printWordsConcise = sentence -> {
+            Arrays.asList(sentence.split(" ")).forEach(s -> System.out.println(s));
+        };
+
+        printWordsConcise.accept("Let's split this up into an array");
+    }
+}
